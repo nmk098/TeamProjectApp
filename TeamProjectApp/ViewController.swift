36f23e2f1781bfd9aee806d968070c3c9854d42e
nmk098 <nkuryumov@gmail.com>
2022-12-01 16:26:49 +0400

    Initial Commit

---
+++ TeamProjectApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TeamProjectApp
+//
+//  Created by Никита Курюмов on 01.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
