ff3cdbdb3030d3e23f28050e599410557cedf105
Aroslav Kochkin <kochkin.2328@inbox.ru>
2022-12-07 22:08:24 +0700

    adding logic LoginVC and update loginStoryboard

--- TeamProjectApp/LogInViewController.swift
+++ TeamProjectApp/LogInViewController.swift
@@ -14,11 +14,66 @@ class LogInViewController: UIViewController {
     @IBOutlet var userName: UITextField!
     @IBOutlet var passwordTF: UITextField!
    
+    private let users = Person.getProfile()
+    private var person: Person!
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard segue.destination is MainScreenViewController else {  return }
+        
     }
 
+    
     @IBAction func logInButtonPressed() {
+        guard checkLoginPassword() else {
+            showAlert(
+                withTitle: "Упс!",
+                andMessage: "Неверное имя или пароль, ну есть же подсказки ;)"
+            )
+            clearTextFields()
+            return
+        }
+        performSegue(withIdentifier: "openMainScreen", sender: nil)
+    }
+}
+
+
+
+extension LogInViewController {
+    
+    // MARK: - TouchesBegan
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    // MARK: - UIAlertController
+    
+    private func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+    
+    // MARK: - Private metods
+    
+    private func checkLoginPassword() -> Bool {
+        guard let login = userName.text else {
+            return false
+        }
+        guard let persone = users[login] else { return false }
+        
+        if persone.password == passwordTF.text {
+            person = persone.person
+            return true
+        }
+        return false
+    }
+    
+    private func clearTextFields() {
+        userName.text = nil
+        passwordTF.text = nil
     }
 }
 
