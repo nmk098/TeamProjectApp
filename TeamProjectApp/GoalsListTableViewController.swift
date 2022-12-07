603608aa1c85e9b343b4cfb0eec3c0443fe635d7
nmk098 <nkuryumov@gmail.com>
2022-12-07 22:17:50 +0400

    First working version

--- TeamProjectApp/GoalsListTableViewController.swift
+++ TeamProjectApp/GoalsListTableViewController.swift
@@ -16,17 +16,26 @@ class GoalsListTableViewController: UITableViewController {
         
     }
         // MARK: - Table view data source
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        }
-        
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
             goalsList.count
         }
         
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-         let cell = tableView.dequeueReusableCell(withIdentifier: "GoalCell", for: <#T##IndexPath#>)
-        
+         let cell = tableView.dequeueReusableCell(withIdentifier: "GoalCell", for: indexPath)
+         let goal = goalsList[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = goal.goalTitle
+        content.secondaryText = goal.goalDescription
+        content.image = UIImage(named: "star")
+        cell.contentConfiguration = content
         return cell  
+    }
+    
+    // MARK: Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        120
     }
        
     @IBAction func cancelButtonTapped(_ sender: Any) {
